1141b931043b088be57d63aad99c368a9ce50026
bertinma <bertin_ma@icloud.com>
2023-05-23 01:20:52 +0200

    add success page, image and rediction

--- app.js
+++ app.js
@@ -31,39 +31,22 @@ app.post('/submit-form', (req, res) => {
     const { firstName, lastName, email, address, photograph, size, frame} = req.body;
     
     // Prepare the SQL query to insert form data into a table
-    const sql = 'USE; INSERT INTO orders_him (first_name, last_name, email, address, photograph, size, frame) VALUES (?, ?, ?, ?, ?, ?, ?)';
-    const values = [firstName, lastName, email, address, photograph, size, frame];
+    const sql = 'INSERT INTO orders.orders_him (first_name, last_name, email, address, photograph, size, frame) VALUES (?, ?, ?, ?, ?, ?, ?);';
+    const values = [firstName, lastName, email, address, photograph, size, (String(frame).toLowerCase() == "true")];
     
     // Execute the SQL query
-    connection.query(sql, values, (err) => {
+    connection.query(sql, values, (err, result, fiels) => {
         if (err) {
             console.error('Error inserting data:', err);
             res.status(500).send('Error inserting data');
         } else {
-            console.log('Data inserted successfully');
-            res.status(200).send('Data inserted successfully');
+            // console.log('Data inserted successfully');
+            // res.status(200).send('Data inserted successfully');
+            const orderId = result.insertId;
+            res.redirect(`/success.html?order=${orderId}`);
         }
     });
 });
-
-// // Define a route to get the count of IDs from a table
-// app.get('/id-count', (req, res) => {
-//     // Prepare the SQL query to get the count of IDs
-//     const sql = 'SELECT COUNT(id) as idCount FROM your_table_name';
-
-//     // Execute the SQL query
-//     connection.query(sql, (err, results) => {
-//         if (err) {
-//             console.error('Error retrieving ID count:', err);
-//             res.status(500).send('Error retrieving ID count');
-//         } else {
-//             const idCount = results[0].idCount;
-//             console.log('ID count:', idCount);
-//             res.status(200).json({ count: idCount });
-//         }
-//     });
-// });
-
 
 // Serve the HTML file as the root URL response
 app.get('/', (req, res) => {
